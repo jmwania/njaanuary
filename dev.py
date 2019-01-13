c5910a3b6866e551c15599c8f01ab58f4acef5b9
jmwania <josphatmwania237@gmail.com>
2019-01-12 19:08:34 -0800

    Employee

---
+++ dev.py
@@ -0,0 +1,14 @@
+class Developer:
+    pass
+
+    def employee(self, name, age, salary):
+        self.age = age
+        self.salary = salary
+
+employee = 'Josphat mwania'
+salary = 80,oooo
+age = 25
+
+self.name()
+self.age()
+self.salary()
